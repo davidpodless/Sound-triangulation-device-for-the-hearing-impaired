327f07a0921ee01031f6b07bd17ea0c7f66f424f
ZedLeppelin <tamir.nahum@mail.huji.ac.il>
2018-12-03 09:38:04 +0200

    Uploading code that is using pyaudio for raspberry use

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from usb_4_mic_array.tuning import Tuning
 import usb.core
 import usb.util
 import time
+import pyaudio
 
 dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
+
+p = pyaudio.PyAudio()
+# info = p.get_host_api_info_by_index(0)
+# numdevices = info.get('deviceCount')
 
 if __name__ == '__main__':
     if dev:
