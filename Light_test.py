2aff68fd930b8702b6d47d2f599615b8ab4df114
ZedLeppelin <tamir.nahum@mail.huji.ac.il>
2018-12-03 08:55:58 +0200

    updated the readme file

---
+++ Light_test.py
@@ -0,0 +1,24 @@
+import time
+from pixel_ring import pixel_ring
+
+
+if __name__ == '__main__':
+    pixel_ring.change_pattern('echo')
+    while True:
+
+        try:
+            pixel_ring.wakeup()
+            time.sleep(3)
+            pixel_ring.think()
+            time.sleep(3)
+            pixel_ring.speak()
+            time.sleep(6)
+            pixel_ring.off()
+            time.sleep(3)
+        except KeyboardInterrupt:
+            pixel_ring.wakeup()
+            break
+
+
+    pixel_ring.off()
+    time.sleep(1)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Sound-triangulation-device-for-the-hearing-impaired
+- For Installing the ReSpeaker we used the guide in [a link]http://wiki.seeedstudio.com/ReSpeaker_Mic_Array_v2.0/
+- We used raspbian for the raspberry
+- We used ReSpeaker Mic Array v2.0
+- use the git [link]https://github.com/respeaker/usb_4_mic_array.git to control the ReSpeaker
