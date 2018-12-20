90b17d168a11df62c68dcfb1782dbf3e76893b94
Ori <oria@cs.huji.ac.il>
2018-12-20 18:01:37 +0200

    start of computing thread

--- computing.py
+++ computing.py
@@ -2,28 +2,47 @@ from usb_4_mic_array.tuning import Tuning
 import usb.core
 import usb.util
 import time
-import pyaudio
+# import pyaudio
 import wave
 import numpy as np
 # from scipy.io import wavfile
 import binascii
 import main
+import matplotlib.pyplot as plt
+
 
 def extractAndCompute(frames, results):
 	while True:
+		nextSample = 0
 		if(frames):
-			nextSample = frames.pop()
-			if(nextSample):
-				channel1 = np.frombuffer(nextSample[2::6], int32)
-				channel2 = np.frombuffer(nextSample[3::6], int32)
-				channel3 = np.frombuffer(nextSample[4::6], int32)
-				channel4 = np.frombuffer(nextSample[5::6], int32)
-				results.appendleft(compute(channel1, channel2, channel3, channel4))
-			else:
-				continue
+			while type(nextSample) == int:
+				nextSample = frames.pop()
+			# if(nextSample != 0):
+			channel1 = nextSample[2::6]
+			channel2 = nextSample[3::6]
+			channel3 = nextSample[4::6]
+			channel4 = nextSample[5::6]
+
+			channel1 = nextSample[1::2]
+			channel2 = nextSample[2::2]
+			if(type(nextSample) != np.ndarray):
+				channel1 = np.frombuffer(nextSample[2::6], int16)
+				channel2 = np.frombuffer(nextSample[3::6], int16)
+				channel3 = np.frombuffer(nextSample[4::6], int16)
+				channel4 = np.frombuffer(nextSample[5::6], int16)
+			results.appendleft(compute(channel1, channel2, channel3, channel4))
+			# else:
+			# 	continue
 		else:
-			continue
+			break
 
 
 def compute(channel1, channel2, channel3, channel4):
-	freq1 = np.fft(channel1)
+	print(channel1)
+	b=[(ele/2**8.)*2-1 for ele in channel1]
+	freq1 = np.fft.fft2(b)
+	# print(freq1)
+	d = (len(freq1)) // 2
+	plt.plot(abs(freq1))
+	# plt.xlim(-len(freq1), len(freq1))
+	plt.show()
--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import threading
 from collections import deque
+import wave
+from scipy.io import wavfile
 
-import recording
+# import recording
 import computing
 
 RESPEAKER_RATE = 16000
 CHUNK = 1024
 RECORD_SECONDS = 32
-RECORD_BUFFER_MAX = (RESPEAKER_RATE / CHUNK) * RECORD_SECONDS
+RECORD_BUFFER_MAX = (RESPEAKER_RATE * RECORD_SECONDS) // CHUNK
+
+def fake_record(file, frames):
+	fs, data = wavfile.read(file)
+	frames.appendleft(data)
+
 
 if __name__ == '__main__':
+	print(RECORD_BUFFER_MAX)
 	frames = deque(RECORD_BUFFER_MAX*[0], RECORD_BUFFER_MAX)
 	results = deque(RECORD_BUFFER_MAX*[0], RECORD_BUFFER_MAX)
 	keepRecording = True
-	recordingThread = threading.Thread(group=None, target=recording.record, name="recording thread", args=(frames))
+	# recordingThread = threading.Thread(group=None, target=recording.record, name="recording thread", args=(frames))
 	computingThread = threading.Thread(group=None, target=computing.extractAndCompute, name="compute thread", args=(frames, results))
-
-	recordingThread.run()
+	fake_record('./output2.wav', frames)
+	# recordingThread.run()
 	computingThread.run()
