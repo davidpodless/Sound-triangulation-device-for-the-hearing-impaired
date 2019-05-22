34f8dca46a48f694350470035718754c2ce1e253
ZedLeppelin <tamir.nahum@mail.huji.ac.il>
2019-05-23 02:12:48 +0300

    test 6

--- graphics/DataCollectingThread.py
+++ graphics/DataCollectingThread.py
@@ -8,6 +8,7 @@ def add_angle_and_time(angle_and_amp):
 	print('DataCollectingThread.angle_list')
 	print(DataCollectingThread.angle_list)
 
+
 def add_angles():
 	while DataCollectingThread.data[0]:
 		toPrint = DataCollectingThread.data[0].pop()
@@ -18,7 +19,7 @@ def add_angles():
 			continue
 		if toPrint[0] != 0:
 			if (toPrint[0][1] != 0):
-				angle_and_amp = (toPrint[0][0], toPrint[0][1]/toPrint[0][1])
+				angle_and_amp = (toPrint[0][0], toPrint[0][1] / toPrint[0][1])
 				add_angle_and_time(angle_and_amp)
 
 
@@ -35,7 +36,7 @@ class DataCollectingThread(threading.Thread):
 		while True:
 			add_angles()
 			self.check_angles_and_time()
-			time.sleep(1/30)
+			time.sleep(1 / 30)
 
 	def check_angles_and_time(self):
 		for prev_time_ind in range(len(DataCollectingThread.time_list)):
@@ -46,8 +47,10 @@ class DataCollectingThread(threading.Thread):
 				del DataCollectingThread.angle_list[prev_time_ind]
 
 			else:
-				DataCollectingThread.angle_list[prev_time_ind] = (DataCollectingThread.angle_list[prev_time_ind][0], DataCollectingThread.angle_list[prev_time_ind][1] * time_passed/3 )
-
+				print(DataCollectingThread.angle_list)
+				DataCollectingThread.angle_list[prev_time_ind] = \
+					(DataCollectingThread.angle_list[prev_time_ind][0],
+					 DataCollectingThread.angle_list[prev_time_ind][1] * ( time_passed / 3))
 
 
 def angle_list():
