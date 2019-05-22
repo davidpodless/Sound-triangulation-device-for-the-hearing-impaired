bed2d5034a24a61240eb2511b5d85fd64d71551f
ZedLeppelin <tamir.nahum@mail.huji.ac.il>
2019-05-22 19:26:57 +0300

    second thread

--- graphics/DataCollectingThread.py
+++ graphics/DataCollectingThread.py
@@ -3,6 +3,10 @@ import time
 import random
 
 
+def add_angle_and_time(angle_and_amp):
+	DataCollectingThread.angle_list.append((angle_and_amp, time.time()))
+
+
 class DataCollectingThread(threading.Thread):
 	angle_list = []
 
@@ -11,8 +15,13 @@ class DataCollectingThread(threading.Thread):
 			DataCollectingThread.angle_list = []
 			for _ in range(4):
 				pair = (random.randint(0, 360), random.uniform(0, 3))
-				DataCollectingThread.angle_list.append(pair)
-			time.sleep(1)
+				add_angle_and_time(pair)
+			time.sleep(0.25)
+
+	def check_angles_and_time(self):
+		for angle in DataCollectingThread.angle_list:
+			continue
+
 
 
 def angle_list():
