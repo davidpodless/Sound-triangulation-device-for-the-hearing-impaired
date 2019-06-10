6732dbb918dfb67974744868e05c25104a7e3c5e
ZedLeppelin <tamir.nahum@mail.huji.ac.il>
2019-06-10 14:16:11 +0300

    test 6

--- graphics/GraphicRunnerThread.py
+++ graphics/GraphicRunnerThread.py
@@ -1,8 +1,9 @@
 import threading
 import graphics.SoundCircle as SoundCircle
-
+from kivy.core.window import Window
 
 class GraphicRunnerThread(threading.Thread):
 	def run(self):
+		Window.fullscreen = True
 		self.graphicThread = SoundCircle.SoundCircleApp()
 		self.graphicThread.run()
